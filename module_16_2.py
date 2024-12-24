c6a4892f7ca4614072fee1e960aa25e98ca89869
xksyl <egorka2004212@gmail.com>
2024-12-24 17:55:00 +0500

    module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -4,19 +4,19 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get('/')
-async def main_page():
+async def main_page() -> str:
     return {'message': 'Главная страница'}
 
 @app.get("/user/admin")
-async def us_ad() -> dict:
+async def us_ad() -> str:
     return (f"Вы вошли как администратор")
 
 
 @app.get("/user/{user_id}")
-async def user_id_page(user_id: int = Path(ge=1, le=100, description='Enter User ID')) -> dict:
+async def user_id_page(user_id: Annotated[
+        int, Path(ge=1, le=100, description='Enter User ID', example=1)]) -> str:
     return (f"Вы вошли как пользователь № {user_id}")
 
 @app.get("/user/{user_name}/{age}")
-async def us_age(user_name: Annotated[str,Path(min_length=5, max_length=20, description='Enter username')],
-                 age:int = Path(ge=18, le=120,description='Enter age') ) -> dict:
+async def us_age(user_name: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> str:
     return (f"Информация о пользователе. Имя: {user_name}, Возраст: {age}")
